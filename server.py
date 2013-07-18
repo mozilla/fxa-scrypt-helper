459d2c566de7320e892afcde4ce39edbf8dc35d4
Sam Penrose <spenrose@mozilla.com>
2013-07-18 15:55:37 -0700

    Takes input from url, should move to request param

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def hello_world(request):
 
 
 def do_scrypt(request):
-    stretched_input = 'stretched_input'
+    stretched_input = bytes(request.matchdict.get('stretched_input', ''))
     salt = 'salt'
     key = scrypt.hash(stretched_input, salt, N=64*1024, r=8, p=1,
                       buflen=1*32)
@@ -18,9 +18,7 @@ def do_scrypt(request):
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('hello', '/hello/{name}')
-    config.add_view(hello_world, route_name='hello')
-    config.add_route('do_scrypt', '/')
+    config.add_route('do_scrypt', '/{stretched_input}')
     config.add_view(do_scrypt, route_name='do_scrypt')
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
