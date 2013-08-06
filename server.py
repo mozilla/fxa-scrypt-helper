0272f5b24468e93e4ed0c42f7556866bca846353
Sam Penrose <spenrose@mozilla.com>
2013-08-06 08:26:43 -0700

    require parameter values to be passed in query string, while restricting

--- server.py
+++ server.py
@@ -4,11 +4,40 @@ from pyramid.response import Response
 
 import scrypt
 
+
+def validate_parameters(request):
+    '''
+    Enforce restrictions on the scrypt parameters.
+    '''
+    valid = {'salt': b'identity.mozilla.com/picl/v1/scrypt',
+             'N': 65536, # aka 'n'
+             'r': 8,
+             'p': 1,
+             'buflen': 32}
+    for parameter in valid:
+        typecaster = bytes if parameter == 'salt' else int
+        input = request.params.get(parameter)
+        if input is None:
+            if parameter == 'N':
+                input = request.params.get('n')
+            if input is None:
+                return (False, 'Missing Scrypt parameter "%s"' % parameter)
+        try:
+            assert typecaster(input) == valid[parameter]
+        except Exception:
+            msg = 'Bad value "%s" for Scrypt parameter "%s"'
+            return (False, msg % (input, parameter))
+    return (True, valid)
+    
+
 def do_scrypt(request):
     stretched_input = bytes(request.matchdict.get('stretched_input', ''))
-    salt = b"identity.mozilla.com/picl/v1/scrypt"
-    key = scrypt.hash(stretched_input, salt, N=64*1024, r=8, p=1,
-                      buflen=1*32)
+    valid, parameters = validate_parameters(request)
+    if not valid:
+        response = Response(parameters) # an error message
+        response.status = 400
+        return response
+    key = scrypt.hash(stretched_input, **parameters)
     return Response(key)
 
 
