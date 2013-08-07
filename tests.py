f6730e24caaf2f7edc168d0ba33d303c2dd4d61d
Sam Penrose <spenrose@mozilla.com>
2013-08-06 20:37:39 -0700

    PEP8

--- tests.py
+++ tests.py
@@ -2,22 +2,28 @@ import unittest
 
 import server as S
 
+
 class TestInputValidation(unittest.TestCase):
 
     def test_validate_parameters(self):
         SALT = b'identity.mozilla.com/picl/v1/scrypt'
         self.assert_(S.validate_parameters(
-            {'N': 64*1024, 'r':8, 'p':1, 'buflen':32, 'salt':SALT}))
-        self.assertRaises(ValueError, S.validate_parameters,
-                          {'N': 32*1024, 'r':8, 'p':1, 'buflen':32, 'salt':SALT})
-        self.assertRaises(ValueError, S.validate_parameters,
-                          {'N': 64*1024, 'r':7, 'p':1, 'buflen':32, 'salt':SALT})
-        self.assertRaises(ValueError, S.validate_parameters,
-                          {'N': 64*1024, 'r':8, 'p':2, 'buflen':32, 'salt':SALT})
-        self.assertRaises(ValueError, S.validate_parameters,
-                          {'N': 64*1024, 'r':8, 'p':1, 'buflen':33, 'salt':SALT})
-        self.assertRaises(ValueError, S.validate_parameters,
-                          {'N': 64*1024, 'r':8, 'p':1, 'buflen':32, 'salt':SALT+"not"})
+            {'N': 64*1024, 'r': 8, 'p': 1, 'buflen': 32, 'salt': SALT}))
+        self.assertRaises(
+            ValueError, S.validate_parameters,
+            {'N': 32*1024, 'r': 8, 'p': 1, 'buflen': 32, 'salt': SALT})
+        self.assertRaises(
+            ValueError, S.validate_parameters,
+            {'N': 64*1024, 'r': 7, 'p': 1, 'buflen': 32, 'salt': SALT})
+        self.assertRaises(
+            ValueError, S.validate_parameters,
+            {'N': 64*1024, 'r': 8, 'p': 2, 'buflen': 32, 'salt': SALT})
+        self.assertRaises(
+            ValueError, S.validate_parameters,
+            {'N': 64*1024, 'r': 8, 'p': 1, 'buflen': 33, 'salt': SALT})
+        self.assertRaises(
+            ValueError, S.validate_parameters,
+            {'N': 64*1024, 'r': 8, 'p': 1, 'buflen': 32, 'salt': SALT+"not"})
 
 
 if __name__ == '__main__':
