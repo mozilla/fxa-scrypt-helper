d09841cc4a4574af72d7cfa7c43b1b10a5bc0d55
Ryan Kelly <rkelly@mozilla.com>
2013-08-19 15:33:28 +1000

    Add CORS headers in nginx config.

--- awsboxen/scripts/build_webserver_node.sh
+++ awsboxen/scripts/build_webserver_node.sh
@@ -85,6 +85,16 @@ http {
     server {
         listen       80 default;
         location / {
+            if (\$request_method = 'OPTIONS') {
+                add_header 'Access-Control-Allow-Origin' '*';
+                add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+                add_header 'Access-Control-Max-Age' 1728000;
+                add_header 'Content-Length' 0;
+                return 204;
+            }
+            add_header 'Access-Control-Allow-Origin' '*';
+            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+            add_header 'Access-Control-Max-Age' 1728000;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
             proxy_set_header Host \$http_host;
             proxy_redirect off;
